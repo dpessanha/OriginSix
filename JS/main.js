9ab6650401dec8fa987527639bce46943a395a34
dpessanha <daopessanha@gmail.com>
2021-07-09 11:39:04 -0300

    aula02

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const link = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
 for (const e of toggle) {
   e.addEventListener('click', function () {
@@ -13,3 +15,14 @@ for (const e of link) {
     nav.classList.remove('show')
   })
 }
+
+// Mudar o header da página quando der scroll
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // scroll é menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
