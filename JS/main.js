93232555ff984f6ddb27f822e8f36385eaf42b4b
dpessanha <daopessanha@gmail.com>
2021-07-11 14:46:49 -0300

    final

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,7 @@ const toggle = document.querySelectorAll('nav .toggle')
 const link = document.querySelectorAll('nav ul li a')
 const header = document.querySelector('#header')
 const btTop = document.querySelector('.back-to-top')
+const sections = document.querySelectorAll('main section[id]')
 const navHeight = header.offsetHeight
 
 for (const e of toggle) {
@@ -17,21 +18,6 @@ for (const e of link) {
   })
 }
 
-// Mudar o header da página quando der scroll
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    // scroll é maior que a altura do header
-    header.classList.add('scroll')
-    // Botão voltar para o topo
-    btTop.classList.remove('hidden')
-  } else {
-    // scroll é menor que a altura do header
-    header.classList.remove('scroll')
-    // Botão voltar para o topo
-    btTop.classList.add('hidden')
-  }
-})
-
 // Testimonials carousel slider Swiper
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -44,6 +30,12 @@ const swiper = new Swiper('.swiper-container', {
   autoplay: {
     delay: 4500,
     disableOnInteraction: false,
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
   },
 })
 
@@ -66,3 +58,42 @@ scrollReveal.reveal(
 `,
   { interval: 100 }
 )
+
+// Link ativo no menu
+function activeMenuLink() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (let section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const startCheckpoint = checkpoint >= sectionTop
+    const endCheckpoint = checkpoint <= sectionTop + sectionHeight
+
+    if (startCheckpoint && endCheckpoint) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// Mudar o header da página quando der scroll
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+    // Botão voltar para o topo
+    btTop.classList.remove('hidden')
+    // Marcar link como ativo na sessão
+    activeMenuLink()
+  } else {
+    // scroll é menor que a altura do header
+    header.classList.remove('scroll')
+    // Botão voltar para o topo
+    btTop.classList.add('hidden')
+  }
+})
