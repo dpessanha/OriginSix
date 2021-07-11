e272b993078ef4e4492c733eb8096236c8e9bafd
dpessanha <daopessanha@gmail.com>
2021-07-11 09:00:20 -0300

    aula04

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,7 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const link = document.querySelectorAll('nav ul li a')
 const header = document.querySelector('#header')
+const btTop = document.querySelector('.back-to-top')
 const navHeight = header.offsetHeight
 
 for (const e of toggle) {
@@ -21,20 +22,29 @@ window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
+    // Botão voltar para o topo
+    btTop.classList.remove('hidden')
   } else {
     // scroll é menor que a altura do header
     header.classList.remove('scroll')
+    // Botão voltar para o topo
+    btTop.classList.add('hidden')
   }
 })
 
 // Testimonials carousel slider Swiper
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
+  spaceBetween: 30,
   pagination: {
     el: '.swiper-pagination',
   },
   mousewheel: true,
   keyboard: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
 })
 
 // ScrollReveal: mostrar elementos conforme a seção aparece.
@@ -51,7 +61,8 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials testimonials,
-  #contact .text, #contact .button 
+  #contact .text, #contact .button,
+  footer .brand, footer .social
 `,
   { interval: 100 }
 )
